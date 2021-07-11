b3ef664eaa56b598891e8e96c84e2273ffa9bf6e
pseudo2020 <20pseudo20@gmail.com>
2021-07-11 14:02:04 +0100

    First commit

--- target/debug/deps/dit-9796a70af206f72a.d
+++
@@ -1,5 +0,0 @@
-/home/jeo/Programming/dit/target/debug/deps/dit-9796a70af206f72a: src/main.rs
-
-/home/jeo/Programming/dit/target/debug/deps/dit-9796a70af206f72a.d: src/main.rs
-
-src/main.rs:
--- target/debug/dit.d
+++
@@ -1,1 +0,0 @@
-/home/jeo/Programming/dit/target/debug/dit: /home/jeo/Programming/dit/src/main.rs
